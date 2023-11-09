c3e15c59ae732557710b8bc5b055878029966db2
Kaue Fulgencio <Kauemfulgencio@outlook.com>
2023-11-09 20:16:46 -0300

    modelo produto e categoria

---
+++ aguanaboca/admin/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    imagem = models.ImageField(upload_to='imagens/')
+
+    def __str__(self):
+        return self.nome
